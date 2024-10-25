0d6b9ef05330066cde17effbaccdaf5aa9a9a226
Joshua Dechant <josh@codecrafting.org>
2024-10-25 15:13:53 -0400

    add withdrawal fee to AstriaBridgeableUSDC (#5)

--- script/AstriaBridgeableUSDC.s.sol
+++ script/AstriaBridgeableUSDC.s.sol
@@ -10,13 +10,19 @@ contract AstriaBridgeableUSDCScript is Script {
         uint32 baseChainAssetPrecision = uint32(vm.envUint("BASE_CHAIN_ASSET_PRECISION"));
         string memory baseChainBridgeAddress = vm.envString("BASE_CHAIN_BRIDGE_ADDRESS");
         string memory baseChainAssetDenomination = vm.envString("BASE_CHAIN_ASSET_DENOMINATION");
+        uint256 sequencerWithdrawalFee = vm.envUint("SEQUENCER_WITHDRAWAL_FEE");
+        uint256 ibcWithdrawalFee = vm.envUint("IBC_WITHDRAWAL_FEE");
+        address feeRecipient = vm.envAddress("FEE_RECIPIENT");
 
         vm.startBroadcast(vm.envUint("PRIVATE_KEY"));
         new AstriaBridgeableUSDC(
             bridgeAddress,
             baseChainAssetPrecision,
             baseChainBridgeAddress,
-            baseChainAssetDenomination
+            baseChainAssetDenomination,
+            sequencerWithdrawalFee,
+            ibcWithdrawalFee,
+            feeRecipient
         );
         vm.stopBroadcast();
     }
